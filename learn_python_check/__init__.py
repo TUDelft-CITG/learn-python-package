fad5c7aaa90ab8faddc5e02fce44c78356e193a4
mike-mendoza <mendozalugo@gmail.com>
2023-06-27 12:53:42 +0200

    update files

--- learn_python_check/__init__.py
+++ learn_python_check/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-lear_python_ceg
+learn_python_check_answers
 Authors: Miguel Angel Mendoza-Lugo, Robert Lanzafame, Ahmed Farahat
 
 E-mail:  m.a.mendozalugo@tudelft.nl, R.C.Lanzafame@tudelft.nl, A.Farahat@student.tudelft.nl
--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='learn_python_ceg_test',                           # should match the package folder
-    packages=['learn_python_ceg_test'],                   # should match the package folder
-    version='1.0.2',                              # important for updates
+    name='learn_python_check',                           # should match the package folder
+    packages=['learn_python_check'],                   # should match the package folder
+    version='1.0.0',                              # important for updates
     license='GNU',                                  # should match your chosen license
-    description='learn_python check answers',
+    description='learn python check answers',
     long_description=long_description,              # loads your README.md
     long_description_content_type="text/markdown",  # README.md is of type 'markdown'
     author='Miguel Angel Mendoza-Lugo, Robert Lanzafame, Ahmed Farahat',
-    author_email='mendozalugo@gmail.com',
-    url='https://github.com/mike-mendoza/learn_python_ceg_test', 
+    author_email='m.a.mendozalugo@tudelft.nl',
+    url='https://github.com/TUDelft-CITG/learn-python-package', 
     project_urls = {                                # Optional
-        "Bug Tracker": "https://github.com/mike-mendoza/learn_python_ceg_test/issues"
+        "Bug Tracker": "https://github.com/TUDelft-CITG/learn-python-package/issues"
     },
     install_requires=[ 'pandas'],    # list all packages that your package uses
     keywords=["pypi"], #descriptive meta-data
@@ -31,5 +31,5 @@ setuptools.setup(
         'Programming Language :: Python :: 3.9',
     ],
     
-    download_url="https://github.com/mike-mendoza/learn_python_ceg_test/archive/refs/tags/v1.0.2.tar.gz",
+    download_url="https://github.com/TUDelft-CITG/learn-python-package/archive/refs/tags/v1.0.0.tar.gz",
 )
